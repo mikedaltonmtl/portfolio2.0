e60f528f11d76a51f7aa0077bc3f26d1e39f8c96
mike-dalton <michael.j.dalton@gmail.com>
2024-07-03 16:56:26 -0400

    Complete Skills text

--- app/components/Skills/Skills.jsx
+++ app/components/Skills/Skills.jsx
@@ -3,11 +3,11 @@ import SkillsScroller from './SkillsScroller';
 
 
 const skillsText = [
-  "The Bootcamp gave me plenty of hands-on experience building apps with the fundamentals of HTML, CSS and JavaScript. We learned through building, and developed a series of apps designed  with the React library. I haven’t used Drupal yet, but would be excited to learn. I appreciate that my knowledge may not run deep, but I believe that passion beats experience in the long term.",
-
+  "I appreciate that my knowledge may not run deep, but I believe that passion beats experience in the long term.Having said that, the Bootcamp did give me plenty of hands-on experience building apps with the fundamentals of HTML, CSS and JavaScript. I learned through building, and developed a series of SPAs, designed to introduce me to the prevelant technologies of today.",
+  "With Express Server and Node.js at the back-end, I learned to use jQuery and the Bootstrap CSS framework. I studied SQL (which I loved!), relational databases and normalization, using the PostgreSQL database. Then came the big reveal, React.",
+  "I guess there's a reson that React 'won', and has become the go-to framework for modern web development. It really was a joy to learn and I quickly came to the conclusion that this was the tool I would like to work with in my career.",
   "I'm currently a member of an open-source project group under the New Idea Machine umbrella. We’re developing a word-game app using React and TypeScript within a Next.js framework, and are currently in the process of deploying with AWS.",
-
-  "I am also working as a freelance developer, training AI chatbots to code for DataAnnotation. Although I'm new to this, I find this work fascinating, and am enjoying the insight it’s giving me into the domain of AI, as well taking this opportunity to learn Python."
+  "I am also working as a freelance developer, training AI chatbots to code. Although I'm new to this, I find this work fascinating, and am enjoying the insight it’s giving me into the domain of AI, as well taking this opportunity to learn Python."
 ];
 
 export default function Skills() {
